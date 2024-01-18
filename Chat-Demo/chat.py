eb5d1a6e7d5b4c6decdc1d13211cfabb940774a5
mottimalka <117903589+mottimalka@users.noreply.github.com>
2024-01-18 17:23:29 +0200

    Refactor chat.py and add embedding functionality

--- Chat-Demo/chat.py
+++ Chat-Demo/chat.py
@@ -1,39 +1,52 @@
+import os
 import openai
-from faker import Faker
 
 # set the openai configuration
-openai.api_key = ""
-openai.api_base = ""
 openai.api_type = "azure"
+openai.api_key = ""
 openai.api_version = "2023-07-01-preview"
-deployment_name = "gpt-35-turbo"
+openai.api_base = ""
+LLM_DEPLOYMENT_NAME = "gpt-35-turbo"
+EMBEDDINGS_MODEL_NAME = "text-embedding-ada-002"
+    
+# chat function
+def chat():
+    messages = []
+        
+    messages.append(
+        {
+            "role": "system",
+            "content": "you are AI Assistent to awnser users question:",
+        })
 
+    while True:
+        # get input from user
+        user_input = input("You: ")
 
-messages = []
+        messages.append({"role": "user", "content": user_input})
 
-# get user_data.csv file content
-with open('user_data.csv', 'r') as f:
-    user_data = f.readlines()
+        response = openai.ChatCompletion.create(
+            engine=LLM_DEPLOYMENT_NAME,
+            messages=messages)
 
-    # convert list to string
-    user_data = ''.join(user_data)
+        messages.append({"role": "assistant", "content": response["choices"][0]["message"]["content"]})
+        print(response["choices"][0]["message"]["content"])
 
-messages.append(
-    {
-        "role": "system",
-        "content": "you are AI Assistent to awnser users question about user_data.csv file, here the file content: " + user_data
-    })
+# embedding function
+def embedding():
+    
+    # read user data from file
+    with open("./chat-demo/user_data.csv", "r") as f:
+        file_conent = f.readlines()
+        user_data = ''.join(file_conent)
+    
+    # create embedding
+    embedding_data = openai.Embedding.create(
+        engine=EMBEDDINGS_MODEL_NAME,
+        input=user_data)
 
-while True:
-    # get input from user
-    user_input = input("You: ")
+    # write embedding to file
+    with open("./chat-demo/embedding_data.csv", "w") as f:
+        f.write(str(embedding_data))
 
-    messages.append({"role": "user", "content": user_input})
-
-    response = openai.ChatCompletion.create(
-        engine=deployment_name,
-        messages=messages
-    )
-
-    messages.append({"role": "assistant", "content": response["choices"][0]["message"]["content"]})
-    print(response["choices"][0]["message"]["content"])
+embedding()
